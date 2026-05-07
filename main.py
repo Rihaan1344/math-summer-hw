64f3015c143483c676fb63127f3c2c6a1fc66bf6
Rihaan1344 <syedmrihaan@gmail.com>
2026-05-07 16:24:15 +0530

    added collinearity checking

--- main.py
+++ main.py
@@ -32,7 +32,21 @@ def get_distance(coords_1: list[float], coords_2: list[float]) -> float:
     distance_km = distance_degrees * 111
     return distance_km
 
-def investigate(place1_index: int, place2_index: int, inv_num: int) -> None:
+def check_collinear(p1, p2, p3):
+
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+
+    area = abs(
+        x1*(y2-y3) +
+        x2*(y3-y1) +
+        x3*(y1-y2)
+    ) / 2
+
+    return area
+
+def investigate(place1_index: int, place2_index: int, inv_num: int) -> tuple[float]:
     estimate_point_1 = (data.loc[place1_index, "lon"], data.loc[place1_index, "lat"])
     estimate_point_2 = (data.loc[place2_index, "lon"], data.loc[place2_index, "lat"])
 
@@ -57,6 +71,8 @@ def investigate(place1_index: int, place2_index: int, inv_num: int) -> None:
     st.latex(f"d_{{real}} = {real}")
     st.latex(f"d_{{estimate}} = {estimate}")
     st.latex(f"Error = d_{{real}} - d_{{estimate}} = {real - estimate}")
+
+    return (estimate_point_1, estimate_point_2)
 
 st.title("_Collinearity on Earth: Test it out!_")
 
@@ -87,31 +103,17 @@ if submitted:
         st.write("Maybe try something else?")
 
 if can_calculate:
-    investigate(0, 1, 1)
-    investigate(1, 2, 2)
-    investigate(0, 2, 3)
-    # places = [place1, place2, place3]
-    # estimate_point_1 = (data.loc[0, "lon"], data.loc[0, "lat"])
-    # estimate_point_2 = (data.loc[1, "lon"], data.loc[1, "lat"])
+    retrieve_points = lambda idx: (data.loc[idx, "lon"], data.loc[idx, "lon"])
+    p1, p2, p3 = (retrieve_points(i) for i in range(0, 3))
 
-    # real_point_1 = (data.loc[0, "lat"], data.loc[0, "lon"])
-    # real_point_2 = (data.loc[1, "lat"], data.loc[1, "lon"])
-
-    # st.divider()
-    # st.subheader(f"_Investigation 1:_ {place1} and {place2}")
-    # st.write("When we use the latitudes and longitudes of these places as x and y values, we can use the distance formula,")
-    # st.latex("d = \sqrt{(x_{2} - x_{1})^2 + (y_{2} - y_{1})^2},")
-    # st.write("then we can calculate the distance between the places!")
-    # st.write(
-    #     f"The result of that operation is: {(estimate := get_distance(
-    #         estimate_point_1, estimate_point_2
-    #         ))}km"
-    #     )
-    # st.write("But this does not account for the Earth's curvature. Hence, this is only an estimate.")
-    # st.write(f"Accounting for the curvature, an online tool tells us that the distance is {
-    #     (real := gd(real_point_1, real_point_2).km)
-    # }km")
-    # st.write("So,")
-    # st.latex(f"d_{{real}} = {real}")
-    # st.latex(f"d_{{estimate}} = {estimate}")
-    # st.latex(f"Error = d_{{real}} - d_{{estimate}} = {real - estimate}")
+    st.divider()
+    st.header("_The million-dollar question_: Are they collinear?")
+    st.write("Three collinear points always form a triangle with area 0. We can use that logic to identify collinearity of points")
+    st.write("The formula,  ")
+    st.latex(r"\text{Area} = \frac{1}{2}\left|x_1(y_2-y_3)+x_2(y_3-y_1)+x_3(y_1-y_2)\right|")
+    st.write("gives us the area of any three points on a plane.")
+    st.write(f"Substituting our longitude and latitude values as x and y, we get {(a := check_collinear(p1, p2, p3))} sq. km")
+    if a < 5:
+        st.write("As this value is pretty close to zero, considering measurement errors and the huge distances we're dealing with, we can say that the places are collinear")
+    else:
+        st.write("This value is too big to be considered collinear. Hence we can say that the places are not collinear!")
