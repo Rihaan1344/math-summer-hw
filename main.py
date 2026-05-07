6d4f49fbbee345ed68ed60fd2971c8935e4ac677
Rihaan1344 <syedmrihaan@gmail.com>
2026-05-07 16:38:32 +0530

    Fixed bugs with collinearity

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def investigate(place1_index: int, place2_index: int, inv_num: int) -> None:
     real_point_2 = (data.loc[place2_index, "lat"], data.loc[place2_index, "lon"])
 
     st.divider()
-    st.subheader(f"_Investigation {inv_num}:_ {data.loc[place1_index, "name"]} and {data.loc[place2_index, "name"]}")
+    st.subheader(f"_Investigation {inv_num}:_ {data.loc[place1_index, 'name']} and {data.loc[place2_index, 'name']}")
     st.write("When we use the latitudes and longitudes of these places as x and y values, we can use the distance formula,")
     st.latex("d = \sqrt{(x_{2} - x_{1})^2 + (y_{2} - y_{1})^2},")
     st.write("then we can calculate the distance between the places!")
@@ -64,9 +64,11 @@ def investigate(place1_index: int, place2_index: int, inv_num: int) -> None:
             ))}km"
         )
     st.write("But this does not account for the Earth's curvature. Hence, this is only an estimate.")
-    st.write(f"Accounting for the curvature, an online tool tells us that the distance is {
-        (real := gd(real_point_1, real_point_2).km)
-    }km")
+    real = gd(real_point_1, real_point_2).km
+    st.write(
+        f"Accounting for the curvature, an online tool tells us "
+        f"that the distance is {real} km"
+    )
     st.write("So,")
     st.latex(f"d_{{real}} = {real}")
     st.latex(f"d_{{estimate}} = {estimate}")
@@ -107,7 +109,7 @@ if can_calculate:
     investigate(1, 2, 2)
     investigate(0, 2, 3)
 
-    retrieve_points = lambda idx: (data.loc[idx, "lon"], data.loc[idx, "lon"])
+    retrieve_points = lambda idx: (data.loc[idx, "lon"], data.loc[idx, "lat"])
     p1, p2, p3 = (retrieve_points(i) for i in range(0, 3))
 
     st.divider()
@@ -115,9 +117,9 @@ if can_calculate:
     st.write("Three collinear points always form a triangle with area 0. We can use that logic to identify collinearity of points")
     st.write("The formula,  ")
     st.latex(r"\text{Area} = \frac{1}{2}\left|x_1(y_2-y_3)+x_2(y_3-y_1)+x_3(y_1-y_2)\right|")
-    st.write("gives us the area of any three points on a plane.")
-    st.write(f"Substituting our longitude and latitude values as x and y, we get {(a := check_collinear(p1, p2, p3))} sq. km")
-    if a < 0.01:
+    st.write("gives us the area estimate of any three points on a plane.")
+    st.write(f"Substituting our longitude and latitude values as x and y, we get {(a := check_collinear(p1, p2, p3))} sq. coordinate degrees")
+    if a < 0.1:
         st.write("As this value is pretty close to zero, considering measurement errors and the huge distances we're dealing with, we can say that the places are collinear")
     else:
         st.write("This value is too big to be considered collinear. Hence we can say that the places are not collinear!")
