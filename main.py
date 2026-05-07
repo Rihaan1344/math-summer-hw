43f675edbc7a359798a24154cd126c1cf449acbd
Rihaan1344 <syedmrihaan@gmail.com>
2026-05-07 15:08:21 +0530

    made app

---
+++ main.py
@@ -0,0 +1,117 @@
+import streamlit as st
+import requests
+import pandas as pd
+from geopy.distance import geodesic as gd
+
+can_calculate = False
+
+def get_coords(place):
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {"q": place,
+              "format": "json"}
+    headers = {"User-Agent": "School-Project"}
+    response = requests.get(url, params = params, headers=headers)
+    results = response.json()
+
+    if not results: return None
+
+    data = results[0]
+    lat = data["lat"]
+    lon = data["lon"]
+    bbox = data["boundingbox"]
+    height = float(bbox[1]) - float(bbox[0])
+    width = float(bbox[3]) - float(bbox[2])
+    marker_radius = min(((height + width) / 2) * (10 ** 5),     (10 ** 6))
+
+    return(place, float(lat), float(lon), float(marker_radius))
+
+def get_distance(coords_1: list[float], coords_2: list[float]) -> float:
+    x1, y1 = coords_1
+    x2, y2 = coords_2
+    distance_degrees = ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+    distance_km = distance_degrees * 111
+    return distance_km
+
+def investigate(place1_index: int, place2_index: int, inv_num: int) -> None:
+    estimate_point_1 = (data.loc[place1_index, "lon"], data.loc[place1_index, "lat"])
+    estimate_point_2 = (data.loc[place2_index, "lon"], data.loc[place2_index, "lat"])
+
+    real_point_1 = (data.loc[place1_index, "lat"], data.loc[place1_index, "lon"])
+    real_point_2 = (data.loc[place2_index, "lat"], data.loc[place2_index, "lon"])
+
+    st.divider()
+    st.subheader(f"_Investigation {inv_num}:_ {data.loc[place1_index, "name"]} and {data.loc[place2_index, "name"]}")
+    st.write("When we use the latitudes and longitudes of these places as x and y values, we can use the distance formula,")
+    st.latex("d = \sqrt{(x_{2} - x_{1})^2 + (y_{2} - y_{1})^2},")
+    st.write("then we can calculate the distance between the places!")
+    st.write(
+        f"The result of that operation is: {(estimate := get_distance(
+            estimate_point_1, estimate_point_2
+            ))}km"
+        )
+    st.write("But this does not account for the Earth's curvature. Hence, this is only an estimate.")
+    st.write(f"Accounting for the curvature, an online tool tells us that the distance is {
+        (real := gd(real_point_1, real_point_2).km)
+    }km")
+    st.write("So,")
+    st.latex(f"d_{{real}} = {real}")
+    st.latex(f"d_{{estimate}} = {estimate}")
+    st.latex(f"Error = d_{{real}} - d_{{estimate}} = {real - estimate}")
+
+st.title("_Collinearity on Earth: Test it out!_")
+
+with st.form("get_places"): 
+    st.header("Enter the places you would like to check")
+
+    place1 = st.text_input("Place 1: ")
+    place2 = st.text_input("Place 2: ")
+    place3 = st.text_input("Place 3: ")
+
+    submitted = st.form_submit_button("Submit")
+
+st.divider()
+
+if submitted:
+
+    try:
+        data = pd.DataFrame(
+            data = [get_coords(place1) , get_coords(place2), get_coords(place3)],
+            columns=["name", "lat", "lon", "size"]
+        )
+        print(data)
+        my_map = st.map(data, size = "size")
+        can_calculate = True
+
+    except Exception:
+        st.header("Sorry, you place wasnt found :/")
+        st.write("Maybe try something else?")
+
+if can_calculate:
+    investigate(0, 1, 1)
+    investigate(1, 2, 2)
+    investigate(0, 2, 3)
+    # places = [place1, place2, place3]
+    # estimate_point_1 = (data.loc[0, "lon"], data.loc[0, "lat"])
+    # estimate_point_2 = (data.loc[1, "lon"], data.loc[1, "lat"])
+
+    # real_point_1 = (data.loc[0, "lat"], data.loc[0, "lon"])
+    # real_point_2 = (data.loc[1, "lat"], data.loc[1, "lon"])
+
+    # st.divider()
+    # st.subheader(f"_Investigation 1:_ {place1} and {place2}")
+    # st.write("When we use the latitudes and longitudes of these places as x and y values, we can use the distance formula,")
+    # st.latex("d = \sqrt{(x_{2} - x_{1})^2 + (y_{2} - y_{1})^2},")
+    # st.write("then we can calculate the distance between the places!")
+    # st.write(
+    #     f"The result of that operation is: {(estimate := get_distance(
+    #         estimate_point_1, estimate_point_2
+    #         ))}km"
+    #     )
+    # st.write("But this does not account for the Earth's curvature. Hence, this is only an estimate.")
+    # st.write(f"Accounting for the curvature, an online tool tells us that the distance is {
+    #     (real := gd(real_point_1, real_point_2).km)
+    # }km")
+    # st.write("So,")
+    # st.latex(f"d_{{real}} = {real}")
+    # st.latex(f"d_{{estimate}} = {estimate}")
+    # st.latex(f"Error = d_{{real}} - d_{{estimate}} = {real - estimate}")
